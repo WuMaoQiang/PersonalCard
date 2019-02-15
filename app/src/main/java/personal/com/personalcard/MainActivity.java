b6ec9b7921426ba406a40d703733cb11f28f9879
wumaoqiang <wumaoqiang@vip.qq.com>
2019-02-15 17:04:37 +0800

    添加图片操作

--- app/src/main/java/personal/com/personalcard/MainActivity.java
+++ app/src/main/java/personal/com/personalcard/MainActivity.java
@@ -2,12 +2,18 @@ package personal.com.personalcard;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
 
 import com.blankj.utilcode.util.CrashUtils;
 import com.blankj.utilcode.util.DeviceUtils;
+import com.blankj.utilcode.util.ImageUtils;
 
 import personal.com.personalcard.base.BaseActivity;
 import personal.com.personalcard.utils.UtilCode;
@@ -21,7 +27,29 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        UtilCode.function(this);
-        DeviceUtils.reboot2Recovery();
+        ImageView imageView = findViewById(R.id.img);
+
+        UtilCode.function(this);//工具类测试
+
+        //图片工具类 加文字水印
+        Drawable aaa = getResources().getDrawable(R.drawable.aaa);
+        Bitmap bitmap = ImageUtils.drawable2Bitmap(aaa);
+        Drawable i = getResources().getDrawable(R.drawable.ic_launcher);
+        Bitmap bitmap2 = ImageUtils.drawable2Bitmap(i);
+        //文字水印
+        Bitmap TestWatermark = ImageUtils.addTextWatermark(bitmap, "123", 20, getResources().getColor(R.color.colorAccent), 00.0f, 00.0f);
+//        imageView.setImageBitmap(TestWatermark);
+        //图片水印
+        Bitmap bitmapAddWatermark = ImageUtils.addImageWatermark(bitmap, bitmap2, 1, 11, 120);
+//        imageView.setImageBitmap(bitmapAddWatermark);
+        //灰度图片
+        Bitmap bitmapToGray = ImageUtils.toGray(bitmap2);
+//        imageView.setImageBitmap(bitmapToGray);
+
+        //快速模糊
+        Bitmap bitmapFastBlur = ImageUtils.fastBlur(bitmap, 0.5f, 1);
+        imageView.setImageBitmap(bitmapFastBlur);
+
+
     }
 }
--- app/src/main/java/personal/com/personalcard/base/BaseActivity.java
+++ app/src/main/java/personal/com/personalcard/base/BaseActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Window;
 
 import com.blankj.utilcode.util.LogUtils;
 
@@ -16,5 +17,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
     }
 }
