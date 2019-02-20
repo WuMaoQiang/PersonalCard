382cc3bae55aaf3dced4d8d16969ac4ea802403f
wumaoqiang <wumaoqiang@vip.qq.com>
2019-02-20 18:03:20 +0800

    处理layout_behavior的bug

--- app/src/main/java/personal/com/personalcard/fragment/HomeFirstFragment.java
+++ app/src/main/java/personal/com/personalcard/fragment/HomeFirstFragment.java
@@ -1,15 +1,7 @@
 package personal.com.personalcard.fragment;
 
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
 import personal.com.personalcard.R;
 import personal.com.personalcard.base.BaseFragment;
 
@@ -20,14 +12,11 @@ import personal.com.personalcard.base.BaseFragment;
  */
 public class HomeFirstFragment extends BaseFragment {
 
-    @BindView(R.id.recycle)
-    RecyclerView mRecycle;
-    @BindView(R.id.swipe_refresh_layout)
-    SwipeRefreshLayout mSwipeRefreshLayout;
+
 
     @Override
     public int getLayoutId() {
-        return R.layout.home_first_fragment;
+        return R.layout.home_first;
     }
 
     @Override
